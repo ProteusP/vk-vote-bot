4b50f8c10407e610c10d70f4227b5aa2538371e2
Mac <doremiheev@vk.com>
2025-03-25 19:59:58 +0300

    added config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,17 @@
+package config
 
+import "os"
+
+type Config struct {
+	MattermostURL   string
+	MattermostToken string
+	TarantoolAddr   string
+}
+
+func Load() *Config {
+	return &Config{
+		MattermostURL:   os.Getenv("MATTERMOST_URL"),
+		MattermostToken: os.Getenv("MATTERMOST_TOKEN"),
+		TarantoolAddr:   os.Getenv("TARANTOOL_ADDR"),
+	}
+}
