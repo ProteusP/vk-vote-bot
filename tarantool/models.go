df1d83f1951f7badd7e7da8303483778b25ba3c9
Mac <doremiheev@vk.com>
2025-03-25 20:14:17 +0300

    added comments to models.go

--- tarantool/models.go
+++ tarantool/models.go
@@ -4,7 +4,7 @@ type Vote struct {
 	ID       string
 	Creator  string
 	Question string
-	Options  map[string]string
-	Votes    map[string]string
-	Status   string
+	Options  map[string]int    // {"option": votes_count}
+	Votes    map[string]string // {"user_id": "option"}
+	Status   string            // "active" | "ended"
 }
